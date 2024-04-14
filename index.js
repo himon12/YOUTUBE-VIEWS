e10b63b7f8878281fc5172afd7b9ac5159de6300
himon12 <100027969+himon12@users.noreply.github.com>
2024-04-14 17:08:34 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error executing that command!');
+    }
+});
+
+client.login(process.env.TOKEN);
+        
