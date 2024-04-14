9d257a070c8305b0bb6b519b629d4f74419dada6
himon12 <100027969+himon12@users.noreply.github.com>
2024-04-14 18:41:39 +0545

    Update view.js

--- YouTube views/Commands/view.js
+++ YouTube views/Commands/view.js
@@ -1,1 +1,20 @@
+module.exports = {
+    name: 'view',
+    description: 'Simulates increasing views for content.',
+    execute(message, args) {
+        // Check if the user has provided a content name
+        if (!args[0]) {
+            return message.reply('Please provide a valid content name.');
+        }
+
+        // Get the content name
+        const contentName = args.join(' ');
+
+        // Increase view count for the content
+        increaseViews(contentName);
+
+        // Respond to the user
+        message.reply(`Views increased for ${contentName}!`);
+    },
+};
 
